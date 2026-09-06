9237e98c6eaa6aa527b13b48d4aa1ad7c1b478b2
Dmitry S. Kulyabov <yamadharma@gmail.com>
2026-09-06 19:02:21 +0300

    feat(main): add new repository

--- template/config/script/fix-submodules.sh
+++ template/config/script/fix-submodules.sh
@@ -9,8 +9,10 @@ if [[ $ORIGIN_URL == *"github.com"* ]]; then
 elif [[ $ORIGIN_URL == *"gitverse.ru"* ]]; then
 	git submodule set-url template/report https://gitverse.ru/dharma/academic-laboratory-report-template.git
 	git submodule set-url template/presentation https://gitverse.ru/dharma/academic-presentation-markdown-template.git
+elif [[ $ORIGIN_URL == *"sourcecraft.dev"* ]]; then
+	git submodule set-url template/report https://git@git.sourcecraft.dev/yamadharma/academic-laboratory-report-template.git
+	git submodule set-url template/presentation https://git@git.sourcecraft.dev/yamadharma/academic-presentation-markdown-template.git
 fi
 
 git submodule update --init --recursive
 git submodule sync
-
